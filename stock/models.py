af28e64e66269ad6d9d9d2cdc020a8886b0b15aa
Khasamoh <sharifakhelef@gmail.com>
2023-01-31 08:51:06 +0300

    changing form

---
+++ stock/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+'''class User(models.Model):
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    class Meta:
+        db_table="User"
+'''     
+class Product(models.Model):
+    productName = models.CharField(max_length=50)
+    buyPrice = models.IntegerField()
+    salePrice = models.IntegerField()
+    quantity = models.IntegerField()
+    
+    def __str__(self):
+        return self.productName
+
+class Customer(models.Model):
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    gender = models.CharField(max_length=6)
+    address = models.CharField(max_length=30)
+    phone = models.CharField(max_length=20)
+    def __str__(self):
+        return self.fname
+        
