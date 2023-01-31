dc3374ef11fcf33bee5de4812ab44142f8fd52bb
Khasamoh <sharifakhelef@gmail.com>
2023-01-31 08:51:49 +0300

    changing

--- stock/urls.py
+++ stock/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('product/', views.product, name='product'),
-    path('customer/', views.customer, name='customer'),
-    path('users/', views.users, name='users'),
+    path('product/', views.create, name='product'),
+    path('customer/', views.insert_customer, name='customer'),
+    path('users/', views.insert_user, name='users'),
     path('sales/', views.sales, name='sales'),
     path('salesummary/', views.salesummary, name='salesummary'),
     path('create/', views.create, name="create")
--- stock/views.py
+++ stock/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from .models import Product,Customer
-from .forms import ProductForm
+from .models import *
+from .forms import *
 from django.http import HttpResponse
 from stock.models import *
 
@@ -8,7 +8,11 @@ from stock.models import *
 def index(request):
     return render(request,'index.html')
 def dashboard(request):
-    return render(request,'dashboard.html')
+    Products = Product.objects.all
+    context = {
+         "Products" : Products
+    }
+    return render(request,'dashboard.html',context)
 def product(request):
     return render(request,'product.html')
 def users(request):
@@ -16,71 +20,59 @@ def users(request):
 def customer(request):
     return render(request,'customer.html')
 def sales(request):
-    return render(request,'sales.html')
+    Products = Product.objects.all
+    context = {
+         "Products" : Products
+    }
+    return render(request,'sales.html',context)
 def salesummary(request):
-    return render(request,'salesummary.html')
+    Products = Product.objects.all
+    context = {
+         "Products" : Products
+    }
+    return render(request,'salesummary.html',context)
 
 # Insert user
 def insert_user(request):
-    if request.method == 'POST':
-        fname = request.POST['fname']
-        lname = request.POST['fname']
-        phone = request.POST['phone']
-        email = request.POST['email']
-        username = request.POST['username']
-        password = request.POST['password']
-        data = User(fname=fname,lname=lname,phone=phone,email=email,username=username,password=password)
-        data.save()
-        success = 'User Created'+fname
-        return HttpResponse(success)
-        #return redirect('/')
-    else:
-        return render(request,'users.html')
-    
-# View user   
-def view_user(request):
-    user = User.objects.all()
-    return render(request,'user.html',{'user':User} )
+    Users = User.objects.all
+    form = UserForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect ('user')
+        form = UserForm()
+    context = {
+        "form":form,
+         "Users" : Users
+    }
+    return render(request, 'users.html', context)
     
 # Insert Customer
 def insert_customer(request):
-    if request.method == 'POST':
-        fname = request.POST['fname']
-        lname = request.POST['fname']
-        gender = request.POST['gender']
-        address = request.POST['address']
-        phone = request.POST['phone']
-        data = Customer(fname=fname,lname=lname,gender=gender,address=address,phone=phone)
-        data.save()
-        return redirect('/')
-        
-    else:
-        return render(request,'customer.html')
-    
-# Insert Product
-def insert_product(request):
-    if request.method == 'POST':
-        productName = request.POST['Pname']
-        buyPrice = request.POST['Buyprice']
-        salePrice = request.POST['Saleprice']
-        saleQuantity = request.POST['quantity']
-        data = Product(productName=productName,buyPrice=buyPrice,salePrice=salePrice,saleQuantity=saleQuantity)
-        data.save()
-        return redirect('/')
-        
-    else:
-        return render(request,'pruduct.html')
+    Customers = Customer.objects.all
+    formC = CustomerForm(request.POST)
+    if request.method == "POST":
+        if formC.is_valid():
+            formC.save()
+            return redirect ('customer')
+        formC = CustomerForm()
+    context = {
+        "formC":formC,
+         "Customers" : Customers
+    }
+    return render(request, 'customer.html', context)
     
 # Insert Product
 def create(request):
+    Products = Product.objects.all
     form = ProductForm(request.POST)
-
     if request.method == "POST":
         if form.is_valid():
             form.save()
             return redirect ('product')
-    form = ProductForm()
+        form = ProductForm()
     context = {
-        "form":form
+        "form":form,
+         "Products" : Products
     }
-    return render(request, 'stock/product.html', context)
+    return render(request, 'product.html', context)
